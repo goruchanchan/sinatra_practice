e81dac219f8ab9d08a4b5dd1a63f6fae3245b51f
goruchan <ppcfr481@yahoo.co.jp>
2022-07-13 22:52:18 +0900

    add パブリックディンレクトりの構成変更
    cssファイルをpublicにおいて管理する際に、現状の構成だと、コンテンツ数の帳尻が合わなくなるため調整

--- myapp.rb
+++ myapp.rb
@@ -1,28 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+$memos_path = "./public/memos/"
+
 def parse_memo_detail id
-  f = File.new("./public/#{id}/name.txt")
+  f = File.new("#{$memos_path}#{id}/name.txt")
   name = f.gets
-  f = File.new("./public/#{id}/content.txt")
+  f = File.new("#{$memos_path}#{id}/content.txt")
   content = ""
   f.read(nil,content)
   {:id=>id, :name=>name, :content=>content}
 end
 
 def parse_memo_directories
-  Dir.open('./public/').children.reject{|dir_name| dir_name.include? "del" }
+  Dir.open("#{$memos_path}").children.reject{|dir_name| dir_name.include? "del" }
 end
 
 def create_memo_directory
-  id = Dir.open('./public/').children.size + 1
-  Dir.mkdir("./public/#{id}", 0777)
+  id = Dir.open("#{$memos_path}").children.size + 1
+  Dir.mkdir("#{$memos_path}#{id}", 0777)
   id 
 end
 
 def write_memo id, name, content
-  IO.write("./public/#{id}/name.txt", "#{name}")
-  IO.write("./public/#{id}/content.txt", "#{content}")
+  IO.write("#{$memos_path}#{id}/name.txt", "#{name}")
+  IO.write("#{$memos_path}#{id}/content.txt", "#{content}")
 end
 
 def sanitizing_text text
@@ -30,7 +32,7 @@ def sanitizing_text text
 end
 
 def delete_memo id
-  File.rename("./public/#{id}", "./public/#{id}.del")
+  File.rename("#{$memos_path}#{id}", "#{$memos_path}#{id}.del")
 end
 
 get '/' do 
